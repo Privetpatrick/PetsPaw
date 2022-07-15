c5b43d6e55d9baed6f2c49f663a0f7b7af79f39e
Sasha Gercen <privetpatrick@gmail.com>
2022-07-15 15:41:38 +0300

    css classes rename, bug fixing done

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ let pageTool = {
     activeDom: null,
     pageOpened: 'main',
     otherPages: ['voting', 'breeds', 'gallery'],
-    likesPages: ['likes', 'favorite', 'dislikes'],
-    orderImgURL: 1,
+    likesPages: ['likes', 'favourites', 'dislikes'],
     gridsCollection: {},
     votingPageSettings: {
         imgShowingUrl: 'img/test-cat.png',
@@ -16,10 +15,15 @@ let pageTool = {
         loaderDom: null,
         limit: 10,
         sort: 'Random',
+        idBreedSelected: '',
         sortAscDom: null,
         sortDescDom: null,
         breedsList: {},
         breedsListDom: null,
+    },
+    _clearGrit: function (pageName = this.activeName) {
+        this.gridsCollection[pageName] = false;
+        document.querySelector(`${pageName}-page .grid-wraper`).innerHTML = '';
     },
     _convertData: function (data) {
         let newData = [];
@@ -44,7 +48,7 @@ let pageTool = {
             breedsList.push(obj);
         });
         breedsList = breedsList.sort();
-        let breedSelectDom = document.querySelector('.breeds .breed-select')
+        let breedSelectDom = document.querySelector('.breeds-page .breed-select')
         this.breedsPageSettings.breedsListDom = breedSelectDom;
         breedsList.forEach(elem => {
             option = document.createElement('option')
@@ -54,10 +58,9 @@ let pageTool = {
         });
     },
     createGrid: function (data, pageName = this.activeName) {
-        this.gridsCollection[pageName] = true;
         let gridDomWrapper;
-        if (pageName === 'favorite') {
-            this._createGridFavorite(data)
+        if (pageName === 'favourites') {
+            this._createGridForLikesFavouritesDislikes(data)
         }
         if (pageName === 'likes') {
             this._createLikesDislikesData(data, 'likes')
@@ -66,7 +69,8 @@ let pageTool = {
             this._createLikesDislikesData(data, 'dislikes')
         }
         if (this.otherPages.includes(pageName) && pageName !== 'voting') {
-            gridDomWrapper = document.querySelector(`.${pageName} .grid-wraper`);
+            this.gridsCollection[pageName] = true;
+            gridDomWrapper = document.querySelector(`.${pageName}-page .grid-wraper`);
             gridDomWrapper.innerHTML = '';
             while (data.length > 10) {
                 let gridDiv = document.createElement('div')
@@ -107,14 +111,15 @@ let pageTool = {
         });
         if (likes.length > 0) {
             likes = this._convertData(likes);
-            this._createGridFavorite(likes, 'likes');
+            this._createGridForLikesFavouritesDislikes(likes, 'likes');
         }
         if (dislikes.length > 0) {
             dislikes = this._convertData(dislikes);
-            this._createGridFavorite(dislikes, 'dislikes')
+            this._createGridForLikesFavouritesDislikes(dislikes, 'dislikes')
         }
     },
-    _createGridFavorite: function (data, pageName = this.activeName) {
+    _createGridForLikesFavouritesDislikes: function (data, pageName = this.activeName) {
+        this.gridsCollection[pageName] = true;
         let gridDomWrapper = document.querySelector(`.${pageName}-page .grid-wraper`);
         gridDomWrapper.innerHTML = '';
         while (data.length > 10) {
@@ -131,9 +136,10 @@ let pageTool = {
                 let deleteButton = document.createElement('div')
                 deleteButton.classList.add('button-hover')
                 deleteButton.addEventListener('click', (e) => {
+                    this.gridsCollection[pageName] = false;
                     let id = e.target.previousSibling.classList[0]
                     let imgId = e.target.previousSibling.classList[1]
-                    requestTool.deleteLikeFavoriteDislike(id, pageName);
+                    requestTool.deleteLikeFavouritesDislike(id, pageName);
                     userData._newLog(pageName, imgId);
                     userData._showLogs()
                     e.target.parentElement.remove()
@@ -156,9 +162,10 @@ let pageTool = {
             let deleteButton = document.createElement('div')
             deleteButton.classList.add('button-hover')
             deleteButton.addEventListener('click', (e) => {
+                this.gridsCollection[pageName] = false;
                 let id = e.target.previousSibling.classList[0]
                 let imgId = e.target.previousSibling.classList[1]
-                requestTool.deleteLikeFavoriteDislike(id, pageName);
+                requestTool.deleteLikeFavouritesDislike(id, pageName);
                 userData._newLog(pageName, imgId);
                 userData._showLogs()
                 e.target.parentElement.remove()
@@ -173,7 +180,7 @@ let pageTool = {
         if (this.votingPageSettings.imgDom) {
             this.votingPageSettings.loaderDom.classList.add('block')
             this.votingPageSettings.loaderDom.classList.remove('none')
-            requestTool.getRequest(1, 'Random')
+            requestTool.getRequest('', 1, 'Random')
                 .then(data => {
                     this.votingPageSettings.imgShowingUrl = data[0].url;
                     this.votingPageSettings.imgDom.src = data[0].url;
@@ -187,10 +194,10 @@ let pageTool = {
                 });
         } else {
             this.votingPageSettings.imgDom = document.querySelector('.img-voting img');
-            this.votingPageSettings.loaderDom = document.querySelector('.voting .loader')
+            this.votingPageSettings.loaderDom = document.querySelector('.voting-page .loader')
             this.votingPageSettings.loaderDom.classList.add('block')
             this.votingPageSettings.loaderDom.classList.remove('none')
-            requestTool.getRequest(1, 'Random')
+            requestTool.getRequest('', 1, 'Random')
                 .then(data => {
                     this.votingPageSettings.imgShowingUrl = data[0].url;
                     this.votingPageSettings.imgDom.src = data[0].url;
@@ -214,7 +221,7 @@ let pageTool = {
                 this._defaultActive();
                 this.activeName = target.classList[1].split('-')[0];
                 this.activeDom = target;
-                this.pageOpened = document.querySelector(`.${this.activeName}`);
+                this.pageOpened = document.querySelector(`.${this.activeName}-page`);
                 this._enable(this.pageOpened);
                 this._disable(document.querySelector('.main'));
                 this.activeDom.classList.add('active-nav');
@@ -275,13 +282,14 @@ let pageTool = {
 
 let userData = {
     likes: [],
-    favorite: null,
+    favourites: null,
     dislikes: [],
     logs: [],
     newAction: function (target, type) {
         if (target.parentElement.classList[0] === 'voting-tools') {
             if (type === 'like') {
                 if (pageTool.votingPageSettings.imgId) {
+                    pageTool.gridsCollection[`${type}s`] = false;
                     let id = pageTool.votingPageSettings.imgId;
                     this._newLog(type);
                     this._showLogs();
@@ -292,13 +300,14 @@ let userData = {
                     return;
                 }
             }
-            if (type === 'favorite') {
+            if (type === 'favourite') {
                 if (pageTool.votingPageSettings.imgId) {
+                    pageTool.gridsCollection[`${type}s`] = false;
                     let id = pageTool.votingPageSettings.imgId;
                     this._newLog(type);
                     this._showLogs();
                     pageTool.votingPageSettings.imgId = '';
-                    requestTool.postFavorite(id)
+                    requestTool.postFavourite(id)
                     pageTool.showImg()
                 } else {
                     return;
@@ -306,6 +315,7 @@ let userData = {
             }
             if (type === 'dislike') {
                 if (pageTool.votingPageSettings.imgId) {
+                    pageTool.gridsCollection[`${type}s`] = false;
                     let id = pageTool.votingPageSettings.imgId;
                     this._newLog(type);
                     this._showLogs();
@@ -328,7 +338,7 @@ let userData = {
     _showLogs: function () {
         if (pageTool.otherPages.includes(pageTool.activeName)) {
             this.logs.forEach((elem) => {
-                let logsListDom = document.querySelector(`.${pageTool.activeName} .log-list`);
+                let logsListDom = document.querySelector(`.${pageTool.activeName}-page .log-list`);
                 let time = this._createTime(elem.date);
                 let text = `Image ID: <b>${elem.id}</b> was added to ${elem.type}s`
                 let logDom = document.createElement('div');
@@ -371,23 +381,6 @@ let requestTool = {
     },
     order: ['RANDOM', 'Asc', 'Desc'],
     url: `https://api.thecatapi.com/v1/images/search?limit=10&order=RANDOM`,
-    getRequest: async function (limit = 10, order = 'RANDOM') {
-        let response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}&order=${order}&size=small&has_breeds=true?mime_types=jpg`, {
-            headers: {
-                'Content-type': 'application/json',
-                'x-api-key': this.api
-            },
-        });
-        if (response.ok) {
-            this.data = await response.json();
-            if (limit == 1) {
-                pageTool.votingPageSettings.imgId = this.data[0].id;
-            }
-            return this.data
-        } else {
-            throw new Error(`GET не сработал этот ERROR`);
-        };
-    },
     getAllBreeds: async function () {
         let response = await fetch(`https://api.thecatapi.com/v1/breeds`, {
             headers: {
@@ -402,8 +395,8 @@ let requestTool = {
             throw new Error(`GET не сработал этот ERROR`);
         };
     },
-    getBreedRequest: async function (id) {
-        let response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`, {
+    getRequest: async function (idBreed = pageTool.breedsPageSettings.idBreedSelected, limit = pageTool.breedsPageSettings.limit, order = pageTool.breedsPageSettings.sort) {
+        let response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_id=${idBreed}&limit=${limit}&order=${order}&has_breeds=true?mime_types=jpg&size=small`, {
             headers: {
                 'Content-type': 'application/json',
                 'x-api-key': this.api
@@ -411,7 +404,6 @@ let requestTool = {
         });
         if (response.ok) {
             this.data = await response.json();
-            console.log(this.data)
             return this.data
         } else {
             throw new Error(`GET не сработал этот ERROR`);
@@ -438,13 +430,13 @@ let requestTool = {
             },
         });
         if (response.ok) {
-            userData.favorite = await response.json();
-            return userData.favorite
+            userData.favourites = await response.json();
+            return userData.favourites
         } else {
             throw new Error(`GET не сработал этот ERROR`);
         };
     },
-    getFavorite: async function () {
+    getFavourites: async function () {
         let response = await fetch(`https://api.thecatapi.com/v1/favourites?sub_id=${this.users.user1}`, {
             headers: {
                 'Content-type': 'application/json',
@@ -452,8 +444,8 @@ let requestTool = {
             },
         });
         if (response.ok) {
-            userData.favorite = await response.json();
-            return userData.favorite
+            userData.favourites = await response.json();
+            return userData.favourites
         } else {
             throw new Error(`GET не сработал этот ERROR`);
         };
@@ -473,7 +465,7 @@ let requestTool = {
             body: data,
         });
     },
-    postFavorite: async function (id) {
+    postFavourite: async function (id) {
         let data = {};
         data.sub_id = this.users.user1;
         data.image_id = id;
@@ -487,11 +479,11 @@ let requestTool = {
             body: data,
         });
     },
-    deleteLikeFavoriteDislike: async function (id, type) {
+    deleteLikeFavouritesDislike: async function (id, type) {
         if (type === 'likes' || type === 'dislikes') {
-            type = 'vote'
+            type = 'votes'
         }
-        let response = await fetch(`https://api.thecatapi.com/v1/${type}s/${id}`, {
+        let response = await fetch(`https://api.thecatapi.com/v1/${type}/${id}`, {
             method: 'DELETE',
             headers: {
                 'Content-type': 'application/json',
@@ -514,12 +506,13 @@ requestTool.getAllBreeds()
     })
     .then(() => {
         pageTool.breedsPageSettings.breedsListDom.addEventListener('change', (e) => {
-            let idBreed = pageTool.breedsPageSettings.breedsList[e.target.value];
-            requestTool.getBreedRequest(idBreed);
+            pageTool.breedsPageSettings.idBreedSelected = pageTool.breedsPageSettings.breedsList[e.target.value];
+            requestTool.getRequest()
+            .then(data => pageTool.createGrid(data))
         });
     });
 
-requestTool.getRequest(pageTool.breedsPageSettings.limit, pageTool.breedsPageSettings.sort)
+requestTool.getRequest()
     .then(data => {
         pageTool.createGrid(data, 'breeds');
         pageTool.createGrid(data, 'gallery');
@@ -528,7 +521,7 @@ requestTool.getRequest(pageTool.breedsPageSettings.limit, pageTool.breedsPageSet
 document.querySelector('.limit').addEventListener('change', (e) => {
     let limit = e.target.value.split(' ')[1];
     pageTool.breedsPageSettings.limit = limit;
-    requestTool.getRequest(limit, pageTool.breedsPageSettings.sort)
+    requestTool.getRequest()
         .then(data => {
             pageTool.createGrid(data);
         });
@@ -539,7 +532,7 @@ pageTool.breedsPageSettings.sortAscDom.addEventListener('click', () => {
     if (pageTool.breedsPageSettings.sort === 'Asc') {
         pageTool.breedsPageSettings.sort = 'Random';
         pageTool.breedsPageSettings.sortAscDom.classList.remove('sort1-active')
-        requestTool.getRequest(pageTool.breedsPageSettings.limit, pageTool.breedsPageSettings.sort)
+        requestTool.getRequest()
             .then(data => {
                 pageTool.createGrid(data)
             });
@@ -547,7 +540,7 @@ pageTool.breedsPageSettings.sortAscDom.addEventListener('click', () => {
         pageTool.breedsPageSettings.sort = 'Asc';
         pageTool.breedsPageSettings.sortAscDom.classList.add('sort1-active')
         pageTool.breedsPageSettings.sortDescDom.classList.remove('sort2-active')
-        requestTool.getRequest(pageTool.breedsPageSettings.limit, 'Asc')
+        requestTool.getRequest()
             .then(data => {
                 pageTool.createGrid(data)
             });
@@ -559,7 +552,7 @@ pageTool.breedsPageSettings.sortDescDom.addEventListener('click', () => {
     if (pageTool.breedsPageSettings.sort === 'Desc') {
         pageTool.breedsPageSettings.sort = 'Random';
         pageTool.breedsPageSettings.sortDescDom.classList.remove('sort2-active')
-        requestTool.getRequest(pageTool.breedsPageSettings.limit, pageTool.breedsPageSettings.sort)
+        requestTool.getRequest()
             .then(data => {
                 pageTool.createGrid(data)
             });
@@ -567,7 +560,7 @@ pageTool.breedsPageSettings.sortDescDom.addEventListener('click', () => {
         pageTool.breedsPageSettings.sort = 'Desc';
         pageTool.breedsPageSettings.sortDescDom.classList.add('sort2-active')
         pageTool.breedsPageSettings.sortAscDom.classList.remove('sort1-active')
-        requestTool.getRequest(pageTool.breedsPageSettings.limit, 'Desc')
+        requestTool.getRequest()
             .then(data => {
                 pageTool.createGrid(data)
             });
@@ -576,28 +569,40 @@ pageTool.breedsPageSettings.sortDescDom.addEventListener('click', () => {
 
 document.querySelectorAll('.likes').forEach(buttonLike => {
     buttonLike.addEventListener('click', () => {
-        requestTool.getLikes()
+        if (pageTool.gridsCollection.likes) {
+            return;
+        } else {
+            requestTool.getLikes()
             .then(data => {
                 pageTool.createGrid(data)
             });
+        }
     });
 });
 
 document.querySelectorAll('.dislikes').forEach(buttonDislike => {
     buttonDislike.addEventListener('click', () => {
-        requestTool.getLikes()
+        if (pageTool.gridsCollection.dislikes) {
+            return;
+        } else {
+            requestTool.getLikes()
             .then(data => {
                 pageTool.createGrid(data)
             });
+        }
     });
 });
 
-document.querySelectorAll('.favorite').forEach(buttonFavorite => {
-    buttonFavorite.addEventListener('click', () => {
-        requestTool.getFavorite()
+document.querySelectorAll('.favourites').forEach(buttonFavourites => {
+    buttonFavourites.addEventListener('click', () => {
+        if (pageTool.gridsCollection.favourites) {
+            return;
+        } else {
+            requestTool.getFavourites()
             .then(data => {
                 pageTool.createGrid(data)
             });
+        }
     });
 });
 
@@ -605,6 +610,7 @@ document.querySelector('.voting-tools').addEventListener('click', (e) => {
     let type = e.target.classList[0].split('-')[1]
     userData.newAction(e.target, type)
 })
+
 document.querySelectorAll('.nav').forEach((navigationElement) => {
     navigationElement.addEventListener('mouseover', () => {
         navigationElement.classList.add('hover-nav')
@@ -617,7 +623,7 @@ document.querySelectorAll('.nav').forEach((navigationElement) => {
     navigationElement.addEventListener('click', () => {
         pageTool.openPage(navigationElement);
         // if (pageTool.activeName === 'breeds' || pageTool.activeName === 'gallery') {
-        //     requestTool.getRequest(10, 'Random')
+        //     requestTool.getRequest()
         //         .then(data => {
         //             pageTool.createGrid(data);
         //         });
